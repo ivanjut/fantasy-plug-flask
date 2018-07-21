b0190f1722fd64a9977c50ecb12f2e516c1da7df
Ivan Jutamulia <ivanj@mit.edu>
2018-07-21 17:09:56 -0400

    added routing functionality

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/my_team")
+def my_team():
+    return render_template('my_team.html')
+
+
+@app.route("/news")
+def news():
+    return render_template('news.html')
+
+
+@app.route("/stats")
+def stats():
+    return render_template('stats.html')
+
+
+@app.route("/video")
+def video():
+    return render_template('video.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
