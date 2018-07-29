db22c891a0ec4aef7b544765cac562a968440b5f
Ivan Jutamulia <ivanj@mit.edu>
2018-07-29 18:17:48 -0400

    need to fix routing

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Gets the absolute path to the working directory
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+# Connects our Flask App to our Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
+#####################################################
+#### ROUTING ####
+#####################################################
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -26,6 +39,17 @@ def stats():
 @app.route("/video")
 def video():
     return render_template('video.html')
+
+
+#####################################################
+#### DATABASES ####
+#####################################################
+class Players(db.Model):
+	# Set table name
+	__tablename__ = "players"
+
+	# Create columns of table
+	id = db.Column(db.Integer, primary_key=True)
 
 
 if __name__ == "__main__":
