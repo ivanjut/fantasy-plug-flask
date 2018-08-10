48f2b1e7bb041b2f218c50050d30430e0be8df2d
Ivan Jutamulia <ivanj@mit.edu>
2018-08-10 12:34:19 -0700

    fixed import error

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 # Import models
 from app.models.user import User
 from app.models.league import League
-from app.models.ownership import memberships
+from app.models.ownership import Ownership
 from app.models.player import Player
